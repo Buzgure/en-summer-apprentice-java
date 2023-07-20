1ef09c5547c8eed6343a008884271e2f24867491
Buzgure <tudor.buzgure@stud.ubbcluj.ro>
2023-07-20 16:13:37 +0300

    Get + Post Request

--- src/main/java/com/endava/apprenticeProject/Controller/CustomerController.java
+++ src/main/java/com/endava/apprenticeProject/Controller/CustomerController.java
@@ -3,9 +3,7 @@ package com.endava.apprenticeProject.Controller;
 import com.endava.apprenticeProject.Model.Customer;
 import com.endava.apprenticeProject.Service.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,10 @@ public class CustomerController {
     @GetMapping("/customers")
     public List<Customer> getALlCustomers(){
         return customerService.getAllCustomers();
+    }
+    @PostMapping("/customer")
+    public Customer addCustomer(@RequestBody Customer customer){
+        return customerService.saveCustomers(customer);
     }
 
 }
